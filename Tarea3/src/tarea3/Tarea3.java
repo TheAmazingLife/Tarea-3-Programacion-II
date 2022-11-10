86b256b2538f9f446736b2a21ca19c54038e6ca6
PabloSanhueza1 <Sanhueza.pem@gmail.com>
2022-11-10 20:53:26 -0300

    colocacion objetos en pantalla

--- Tarea3/src/tarea3/Tarea3.java
+++ Tarea3/src/tarea3/Tarea3.java
@@ -3,8 +3,8 @@ package tarea3;
 public class Tarea3 {
 
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
         Ventana Ventana = new Ventana();
+        Ventana.setVisible(true);
     }
 
 }
--- Tarea3/src/tarea3/Ventana.java
+++ Tarea3/src/tarea3/Ventana.java
@@ -1,11 +1,15 @@
 package tarea3;
 
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
+import java.awt.*;
+import javax.swing.*;
 
 public class Ventana extends JFrame {
+
+    public JPanel panel;
+
     public Ventana() {
         crearVentana();
+        iniciarComponentes();
     }
 
     private void crearVentana() {
@@ -23,4 +27,91 @@ public class Ventana extends JFrame {
         // medoto alternativo this.setIconImage(new ImageIcon(this.getClass().getResource("/recursos/iconoVentana.png")).getImage());
     }
 
+    private void iniciarComponentes() {
+        colocarPanel();
+        colocarBotones();
+        colocarEtiquetas();
+    }
+
+    private void colocarPanel() {
+
+        // se coloca el fondo
+        panel = new JPanel();
+        panel.setBackground(Color.lightGray);
+        panel.setLayout(null);
+        this.getContentPane().add(panel);
+    }
+
+    private void colocarEtiquetas() {
+
+        // ventana 1
+        JLabel ventana1 = new JLabel();
+        ventana1.setBounds(60 + 130 * 0, 50, 120, 400);
+        ventana1.setOpaque(true);
+        ventana1.setBackground(Color.cyan);
+        ventana1.setLayout(null);
+        panel.add(ventana1);
+
+        // ventana 2
+        JLabel ventana2 = new JLabel();
+        ventana2.setBounds(60 + 130 * 1, 50, 120, 400);
+        ventana2.setOpaque(true);
+        ventana2.setBackground(Color.cyan);
+        ventana2.setLayout(null);
+        panel.add(ventana2);
+
+        // ventana 3
+        JLabel ventana3 = new JLabel();
+        ventana3.setBounds(60 + 130 * 2, 50, 120, 400);
+        ventana3.setOpaque(true);
+        ventana3.setBackground(Color.cyan);
+        ventana3.setLayout(null);
+        panel.add(ventana3);
+
+        // se coloca la maquina
+        JLabel maquina = new JLabel();
+        maquina.setBounds(250, 10, 350, 500);
+        maquina.setOpaque(true);
+        maquina.setBackground(Color.orange);
+        maquina.setLayout(null);
+        panel.add(maquina);
+
+    }
+
+    private void colocarBotones() {
+
+        // Boton PULL
+        JButton botonPull = new JButton("PULL");
+        botonPull.setBounds(60, 450 + 50, 380, 100);
+        botonPull.setForeground(Color.white);
+        botonPull.setBackground(Color.black);
+        botonPull.setFont(new Font("arial", 1, 20));
+        botonPull.setEnabled(true); // si es false, el boton esta "apagado"
+        botonPull.setMnemonic('p'); // la tecla funciona con alt + letra
+        panel.add(botonPull);
+
+        // boton cocacola
+        JButton botonCocacola = new JButton(new ImageIcon("botonCocacola.png")); // tamaño de imagen ya coincide con tamaño del boton
+        botonCocacola.setBounds(490, 100, 80, 40);
+        botonCocacola.setEnabled(true);
+        botonCocacola.setBackground(Color.red);
+        botonCocacola.setMnemonic('c');
+        panel.add(botonCocacola);
+
+        // boton fanta
+        JButton botonFanta = new JButton(new ImageIcon("botonCocacola.png")); // tamaño de imagen ya coincide con tamaño del boton
+        botonFanta.setBounds(490, 170, 80, 40);
+        botonFanta.setEnabled(true);
+        botonFanta.setBackground(Color.red);
+        botonFanta.setMnemonic('c');
+        panel.add(botonFanta);
+
+        // boton sprite
+        JButton botonSprite = new JButton(new ImageIcon("botonCocacola.png")); // tamaño de imagen ya coincide con tamaño del boton
+        botonSprite.setBounds(490, 240, 80, 40);
+        botonSprite.setEnabled(true);
+        botonSprite.setBackground(Color.red);
+        botonSprite.setMnemonic('c');
+        panel.add(botonSprite);
+    }
 }
